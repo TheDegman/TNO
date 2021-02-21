d7e5cfb637f34fd097e040de352b1eb7b2d1c959
The Degman <dorianmarusic@gmail.com>
2021-02-21 18:55:24 +0100

    ...

---
+++ TNO/TNO/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TNO
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            top_img1.Source = ImageSource.FromResource("TNO.slike.TOP1.png");
+            top_img2.Source = ImageSource.FromResource("TNO.slike.top_1.png");
+            top_img3.Source = ImageSource.FromResource("TNO.slike.top_1.png");
+            desc.Source = ImageSource.FromResource("TNO.slike.description.png");
+            features.Source = ImageSource.FromResource("TNO.slike.fea.png");
+        }
+    }
+}
