1cb6ff5988d0dc536f8e2ad4e8a7487cbff3391c
The Degman <dorianmarusic@gmail.com>
2021-02-23 22:31:36 +0100

    3!

--- TNO/TNO/MainPage.xaml.cs
+++ TNO/TNO/MainPage.xaml.cs
@@ -10,14 +10,45 @@ namespace TNO
 {
     public partial class MainPage : CarouselPage
     {
+        int i = 1;
         public MainPage()
         {
             InitializeComponent();
             top_img1.Source = ImageSource.FromResource("TNO.slike.TOP1.png");
-            top_img2.Source = ImageSource.FromResource("TNO.slike.top_1.png");
-            top_img3.Source = ImageSource.FromResource("TNO.slike.top_1.png");
+           
             desc.Source = ImageSource.FromResource("TNO.slike.description.png");
             features.Source = ImageSource.FromResource("TNO.slike.fea.png");
+            //galerija();
+            Extras.Source = ImageSource.FromResource("TNO.slike.extras.png");
+            
+        }
+        private void galerija()
+        {
+            InitializeComponent();
+            galery.Source = ImageSource.FromResource(string.Format($"TNO.galerija.{i}.jpg"));
+        }
+
+        private void next_Clicked(object sender, EventArgs e)
+        {
+            if (i == 6)
+            {
+                i = 0;
+            }
+            galerija();
+            i++;
+
+        }
+
+        private void previous_Clicked(object sender, EventArgs e)
+        {
+            if (i==0)
+            {
+                i = 6;
+
+            }
+            galerija();
+            i--;
+
         }
     }
 }
